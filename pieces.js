f464066dd450da5c50060dc86b468082551a1263
Axel GMX <agmx183@gmail.com>
2023-08-17 22:27:24 +0200

    ordonner et filtrer les fiches produits grâce aux fonctions sort et filter

--- pieces.js
+++ pieces.js
@@ -5,46 +5,106 @@ const pieces = await reponse.json(); // Parse la réponse en tant qu'objet JavaS
 
 /*("Parse" signifie analyser et transformer des données d'un format à un autre, généralement pour rendre les données utilisables par un programme informatique.)*/
 
-// Sélection du premier article
-const article = pieces[0]; // Sélectionne le premier élément du tableau de pièces
+for (let i = 0; i < pieces.length; i++) {
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // On crée l’élément img.
+    const imageElement = document.createElement("img");
+    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+    imageElement.src = pieces[i].image;
 
-// Création de l'élément d'image
-const imageElement = document.createElement("img");
+    // Idem pour le nom, le prix et la catégorie...
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = pieces[i].nom;
 
-// Accède à la propriété 'image' de l'objet 'article' (la 1ère piece) et affecte sa valeur à la propriété 'src' de l'élément <img>
-imageElement.src = article.image;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
 
-const nomElement = document.createElement("h2"); // Crée un élément <h2> dans le DOM
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
 
-nomElement.innerText = article.nom; // Affecte la valeur de la propriete 'nom' de l'article comme contenu textuel de l'élément <h2> avec la méthode innerText
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment"
 
-const prixElement = document.createElement("p"); // Crée un élément <p> dans le DOM
+    const disponibiliteElement = document.createElement("p");
+    disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock"
 
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35  ? "€" : "€€€"})`; // Utilise une template string (littéraux de gabarit : ils commencent et se terminent toujours par des backticks ``) pour afficher la valeur de la propriete 'prix' de l'article dans le <p>.
-//On affiche aussi un symbole '€' ou '€€€' selon 'article.prix < 35'.
+    // On rattache la balise article à la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    // Idem pour le nom, le prix et la catégorie...
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(disponibiliteElement);
+}
 
-const categorieElement = document.createElement("p"); // Crée un élément <p> dans le DOM
+const boutonTrier = document.querySelector(".btn-trier");
 
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)"; // Affecte la valeur de la propriete 'catégorie' de l'article comme contenu textuel de l'élément <p>, sinon affecte '(aucune catégorie)' comme contenu textuel
+const piecesOrdonnees = Array.from(pieces);//créer une copie de la liste "pieces". Cela permet de ne pas toucher à l'ordre de la liste "pieces" pour que les autre tris et filtres de la page fonctionnent correctement
+boutonTrier.addEventListener("click", function () {
+    piecesOrdonnees.sort(function (a,b) {
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment"
+/****************************************************************************************************************************************
+ * 
+ * La fonction "sort" prend en argument une nouvelle fonction. Nous déclarons celle-ci à l’intérieur des parenthèses de "sort", sans lui donner de nom. On dit qu’elle est anonyme. Cette fonction anonyme sera appelée par "sort" pour comparer deux éléments entre eux.
+ * 
+ * la fonction anonyme prend deux paramètres qu’il faudra comparer pour dire lequel doit être rangé avant l’autre dans la liste réordonnée finale. Traditionnellement, on nomme ces deux paramètres A et B.
+ * 
+ * La fonction "sort" s’attend à recevoir un nombre de la part de la fonction anonyme. Le signe de ce nombre (positif, négatif ou nul) sert à indiquer dans quel ordre ranger les deux éléments :
+    * si le nombre est positif, alors B sera rangé avant A ;
+    * si le nombre est négatif, alors A sera rangé avant B ;
+    * si le nombre est zéro (0), alors l’ordre sera inchangé. 
+ *
+ * La fonction "sort" modifie la liste qu’elle réordonne “en place” (in-place, en anglais). Cela veut dire que les éléments de la liste changent de place. Cela pose un problème car la liste d’origine avec l’ordre d’origine est aussi modifiée. Pour résoudre ce problème, nous pouvons créer une copie de la liste avec la fonction Array.from
+ *
+****************************************************************************************************************************************/
 
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock"
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+// Ajoute un écouteur d'événements pour le clic sur le bouton "boutonFiltrer"
+boutonFiltrer.addEventListener("click", function () {
+   // Crée un nouveau tableau "piecesFiltrees" en filtrant les éléments du tableau "pieces"
+   // Le filtre est basé sur la condition que le prix de chaque "piece" soit inférieur ou égal à 35
+   const piecesFiltrees = pieces.filter(function (piece) {
+       return piece.prix <= 35;
+   });
+   // Affiche le tableau résultant après le filtrage dans la console
+   console.log(piecesFiltrees);
+});
+ /**
+  * 
+  * La fonction filter prend en argument une fonction anonyme qui sera appelée une fois par élément de la liste. 
+  * La fonction anonyme prend un paramètre : l’élément à tester, ici on l'appellera "piece". 
+  * Elle doit retourner une valeur booléenne selon la validité de la condition du "return":
+        * true si l’élément doit se trouver dans la liste filtrée ;
+        * false si l’élément ne doit pas se trouver dans la liste filtrée.
+  * 
+  * Le nouveau tableau appelé piecesFiltrees est créé à partir du tableau existant pieces, en appliquant un filtre basé sur le prix de chaque "piece". Seules les pièces avec un prix inférieur ou égal à 35 "piece.prix <= 35" seront incluses dans ce nouveau tableau.
+  * 
+  * 
+ **/
 
-// Sélectionne l'élément HTML avec la classe "fiches"
-const sectionFiches = document.querySelector(".fiches");
+ const boutonFiltreDescription = document.querySelector(".btn-filtrer-description");
+ boutonFiltreDescription.addEventListener("click", function() {
+    const piecesAvecDescription = pieces.filter(function(piece) {
+        return piece.description;
+    });
+    console.log(piecesAvecDescription);
+ })
 
-// Appelle la méthode appendChild() sur l'élément avec la classe "fiches" (sectionFiches) pour ajouter l'élément <img> (imageElement) en tant qu'enfant
-sectionFiches.appendChild(imageElement);
+ const boutonTriDecroissant = document.querySelector(".btn-trier-decroissant")
+boutonTriDecroissant.addEventListener("click", ()=>{
+    piecesOrdonnees.sort((a,b)=>{
+        return b.prix - a.prix;
+    });
+    console.log(piecesOrdonnees);
 
-sectionFiches.appendChild(nomElement);
-
-sectionFiches.appendChild(prixElement);
-
-sectionFiches.appendChild(categorieElement);
-
-sectionFiches.appendChild(descriptionElement);
-
-sectionFiches.appendChild(disponibiliteElement);
+})
